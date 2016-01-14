9ff469d16148e40fb98075c70d24e93d6606990a
chamberlainrd <chamberlainrd@email.appstate.edu>
2016-01-14 01:16:57 -0500

    moved fxn calls to bottom
    added case structure
    made empty parse fxn
    still need to add back button

--- Gui_Box/MET_Package.m
+++ Gui_Box/MET_Package.m
@@ -5,66 +5,67 @@ function [  ] = MET_Package()
 % Step_One_Figure. See set up code for parameter definitions. 
 % Step_Three_Figure follows step three from the manual, setting z-threshold
 % parameters. Note: rows_per_day is set automatically. 
+guiState = 0;
 global guiStruct;
 persistent settings
 guiStruct = struct();
 %Run figure to get parameters for caller
-F = Step_One_Figure();
-waitfor(F);
-directory = [guiStruct.directory,'\'];
-prefix = guiStruct.prefix;
-datestart = guiStruct.datestart;
-dateend = guiStruct.dateend;
-suffix = guiStruct.suffix;
-ukoln_toggle = guiStruct.ukoln_toggle
-records_per_file = guiStruct.records_per_file;
-if ukoln_toggle == 0
-        MRR = caller_MRR_simp2matrix(directory, prefix, datestart,dateend, suffix, records_per_file);  
-elseif ukoln_toggle == 1
-        MRR = caller_MRR_read_ukoln_nc(directory, prefix, datestart,dateend, suffix, records_per_file);  
-end
+switch guiState
+    case 0
+        F = Step_One_Figure();
+        waitfor(F);
+        directory = [guiStruct.directory,'\'];
+        prefix = guiStruct.prefix;
+        datestart = guiStruct.datestart;
+        dateend = guiStruct.dateend;
+        suffix = guiStruct.suffix;
+        ukoln_toggle = guiStruct.ukoln_toggle
+        records_per_file = guiStruct.records_per_file;
+
 %*****************end step one/two*************
 
 dummer='dummer';
-
+    case 1
 %  call next fig to set up for MRR_Storm_Detection
-F = Step_Three_Figure();
-waitfor(F);
-settings = struct();
-settings.Z_threshhold = guiStruct.Z_threshhold;
-settings.Z_threshhold_2 = guiStruct.Z_threshhold_2;
-settings.rows_per_day = guiStruct.records_per_file;
-[MRR_filtered dates] = MRR_Storm_Detection(MRR, settings);
+        F = Step_Three_Figure();
+        waitfor(F);
+        settings = struct();
+        settings.Z_threshhold = guiStruct.Z_threshhold;
+        settings.Z_threshhold_2 = guiStruct.Z_threshhold_2;
+        settings.rows_per_day = guiStruct.records_per_file;
 
-%************end step three**************
 
-% get parameters for call to  gen_metar2struct.
-F = Step_Four_Figure();
-waitfor(F);
-metarfilepath = guiStruct.metarfilepath;
-savefilepath = guiStruct.savefilepath;
-metartype = guiStruct.metartype;
-%minuteinterval = guiStruct.minuteinterval;
-gen_metar2struct(metarfilepath, savefilepath); 
-%*****************end step four************
+            %************end step three**************
+    case 2
+            % get parameters for call to  gen_metar2struct.
+        F = Step_Four_Figure();
+        waitfor(F);
+        metarfilepath = guiStruct.metarfilepath;
+        savefilepath = guiStruct.savefilepath;
+        metartype = guiStruct.metartype;
+        
+        %minuteinterval = guiStruct.minuteinterval;
 
-%get parameters for  MRR_Add_Storms and calculate_avg, Steps Five and Six
-F = Step_Five_Figure();
-waitfor(F);
-settings.frontpage_orig  = guiStruct.frontpg_filename;
-settings.metarfile_mat = guiStruct.matFilePath;
-settings.empty_stormpage = guiStruct.template_filename;
-[fileName,filePath , ~] = uiputfile('.mat', 'Save As');
-settings.frontpage_saveas = fileName;
-MRR_Add_Storms(MRR_filtered, dates, settings);
-calculate_avgs(settings.frontpage_savas,0,length(dates),0);
-%***************end steps five/six*************
+        %*****************end step four************
 
-%So, we actually have all the data for this step, we can just run with it. 
-htmlfile = guiStruct.frontpg_filename;
-homelocation = guiStruct.home_filepath;
-stormsummarypg = guiStruct.fronpg_filePath;
-add_next_prev(htmlfile,homelocation , stormsummarypg, '');
+    case 3
+        %get parameters for  MRR_Add_Storms and calculate_avg, Steps Five and Six
+        F = Step_Five_Figure();
+        waitfor(F);
+        settings.frontpage_orig  = guiStruct.frontpg_filename;
+        settings.metarfile_mat = guiStruct.matFilePath;
+        settings.empty_stormpage = guiStruct.template_filename;
+        [fileName,filePath , ~] = uiputfile('.mat', 'Save As');
+        settings.frontpage_saveas = fileName;
+
+        %***************end steps five/six*************
+
+        %So, we actually have all the data for this step, we can just run with it. 
+        
+        htmlfile = guiStruct.frontpg_filename;
+        homelocation = guiStruct.home_filepath;
+        stormsummarypg = guiStruct.fronpg_filePath;
+
 % - htmlfile: File name of your frontpage html file.
 % - homelocation: Relative path you want to set for the “Home” button in each storm.  
 % I always make the “Home” button go back to the index of the directory the front
@@ -79,4 +80,28 @@ add_next_prev(htmlfile,homelocation , stormsummarypg, '');
 % ml file somewhere else. 
 % xample:
 
+    case 4
+        %CALL ERROR CHECK FXN HERE%
+        [flag,errorList] = parse_fxn(guiStruct);
+        if flag == 1
+            %do some sort of reporting here
+            %Print list of bad parameters and call figure five
+            display('Unable to parse the following parameters');
+            errorList
+        else
+            if ukoln_toggle == 0
+                    MRR = caller_MRR_simp2matrix(directory, prefix, datestart,dateend, suffix, records_per_file);  
+            elseif ukoln_toggle == 1
+                    MRR = caller_MRR_read_ukoln_nc(directory, prefix, datestart,dateend, suffix, records_per_file);  
+            end
 
+            [MRR_filtered dates] = MRR_Storm_Detection(MRR, settings);
+
+            gen_metar2struct(metarfilepath, savefilepath); 
+
+            MRR_Add_Storms(MRR_filtered, dates, settings);
+            calculate_avgs(settings.frontpage_savas,0,length(dates),0);
+
+            add_next_prev(htmlfile,homelocation , stormsummarypg, '');
+        end
+end
---
+++ Gui_Box/parse_fxn.m
@@ -0,0 +1,8 @@
+function [flag,errorList] = parse_fxn(guiStruct)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+flag = 1;
+errorList = '';
+
+end
+
